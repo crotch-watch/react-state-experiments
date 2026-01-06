1ae1cd0f0af9011bb6fc0a2cad69fe706c24cd0c
crotch-watch <naps_08_bay@icloud.com>
2026-01-06 20:32:49 +0530

    separated view state and setters from data state

--- src/Todo/Todo.hook.ts
+++ src/Todo/Todo.hook.ts
@@ -1,18 +1,18 @@
-import { useReducer } from "react"
+import { useReducer, useState } from "react"
 import { formReducer } from "./Todo.reducer"
 import { DEFAULT_FORM_STATE } from "./Todo.constants"
 import type { Todo } from "./Todo.types"
 
 export const useForm = () => {
-  const [state, dispatch] = useReducer(formReducer, DEFAULT_FORM_STATE)
+  const [todos, dispatch] = useReducer(formReducer, DEFAULT_FORM_STATE)
 
   const createTodo = (todo: Todo) =>
     dispatch({ type: "createTodo", payload: todo })
 
   const editTodo = (todo: Todo) => dispatch({ type: "editing", payload: todo })
 
-  const toggleEdit = (todoId: Todo["uid"]) =>
-    dispatch({ type: "toggleEdit", payload: todoId })
+  // const toggleEdit = (todoId: Todo["uid"]) =>
+  //   dispatch({ type: "toggleEdit", payload: todoId })
 
   const resetTodos = () => dispatch({ type: "resetTodos" })
 
@@ -20,7 +20,21 @@ export const useForm = () => {
     dispatch({ type: "deleteTodo", payload: todoId })
 
   return {
-    state,
-    setters: { createTodo, editTodo, toggleEdit, resetTodos, deleteTodo },
+    todos,
+    setters: { createTodo, editTodo, resetTodos, deleteTodo },
   }
+}
+
+export const useToggleEdit = () => {
+  const [liveEditing, setLiveEditing] = useState<Array<Todo["uid"]>>([])
+
+  const toggleEdit = (id: Todo["uid"]) => {
+    liveEditing.includes(id)
+      ? setLiveEditing((prevState) => prevState.filter((uid) => uid !== id))
+      : setLiveEditing((prevState) => [...prevState, id])
+  }
+
+  const clearLiveEdits = () => setLiveEditing([])
+
+  return { liveEditing, toggleEdit, clearLiveEdits }
 }
