d801bcae1c69336d2d3f9071a3df0760285dece2
crotch-watch <naps_08_bay@icloud.com>
2026-01-29 23:55:46 +0530

    rewrite: effects are moved in useEffect with entry/exit actions

--- src/Async-Load-With-Cancel/Async.hooks.ts
+++ src/Async-Load-With-Cancel/Async.hooks.ts
@@ -1,4 +1,4 @@
-import { useReducer, useRef, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import { asyncReducer } from "./Async.reducer"
 import { asyncEvents, asyncStates, BASE_URL } from "./Async.consts"
 
@@ -7,28 +7,30 @@ const { paramChanged, errored, acknowledged } = asyncEvents
 
 export const useAsync = () => {
   const [state, dispatch] = useReducer(asyncReducer, input)
-  const [posts, setPosts] = useState()
-
-  const abort = useRef<AbortController>(new AbortController())
+  const [param, setParam] = useState("")
+  const [result, setResult] = useState()
 
   const acknowledgeError = () => dispatch(acknowledged)
+  const changeParam = (userId: string) => setParam(userId)
 
-  const emitError = () => dispatch(errored)
+  useEffect(() => {
+    if (state === input) {
+      dispatch(paramChanged)
 
-  const send = (userId: string) => {
-    abort.current.abort()
+      const abortController = new AbortController()
 
-    fetch(`${BASE_URL}?userId=${userId}`, {
-      signal: abort.current.signal,
-    })
-      .then((res) => res.json())
-      .then((data) => setPosts(data))
+      fetch(`${BASE_URL}?=${param}`, { signal: abortController.signal })
+        .then((res) => res.json())
+        .then(setResult)
+        .catch(() => dispatch(errored))
 
-    dispatch(paramChanged)
-  }
+      return () => abortController.abort("aborting previous user request")
+    }
+  }, [state, param])
 
   return {
-    state: { state, posts },
-    action: { acknowledgeError, emitError, send },
+    state: { state },
+    action: { acknowledgeError, changeParam },
+    result,
   }
 }
