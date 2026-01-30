10f5e96247ff8784cf8df8ae10b92c66924d7368
crotch-watch <naps_08_bay@icloud.com>
2026-01-30 22:22:27 +0530

    pushing more data into state for atomic update rather than multiple local state handling and decomposing the state

--- src/Async-Load-With-Cancel/Async.consts.ts
+++ src/Async-Load-With-Cancel/Async.consts.ts
@@ -1,9 +1,13 @@
+import type { AsyncState, Post } from "./Async.types"
+
 export const asyncStates = {
   input: "input",
   error: "error",
 } as const
 
-export type AsyncState = keyof typeof asyncStates
+Object.freeze(asyncStates)
+
+export type AsyncMode = keyof typeof asyncStates
 
 export const asyncEvents = {
   paramChanged: "paramChanged",
@@ -11,6 +15,23 @@ export const asyncEvents = {
   acknowledged: "acknowledged",
 } as const
 
+Object.freeze(asyncEvents)
+
 export type AsyncEvent = keyof typeof asyncEvents
 
+export const DEFAULT_ASYNC_MODE: AsyncMode = asyncStates.input
+export const DEFAULT_PARAM: string = ""
+export const DEFAULT_POSTS: Array<Post> = []
+export const DEFAULT_ERROR: string = ""
+
+export const DEFAULT_ASYNC_STATE: AsyncState = {
+  mode: DEFAULT_ASYNC_MODE,
+  param: DEFAULT_PARAM,
+  posts: DEFAULT_POSTS,
+  errorMessage: DEFAULT_ERROR,
+} as const
+
+Object.freeze(DEFAULT_ASYNC_STATE)
+
 export const BASE_URL = "https://jsonplaceholder.typicode.com/posts"
+export const ABORT_MESSAGE = "aborting previous user request"
