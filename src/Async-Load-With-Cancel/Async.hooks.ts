8597396fcc9327058153373f06ed87bb26eafd8c
crotch-watch <naps_08_bay@icloud.com>
2026-01-30 22:25:11 +0530

    accomodating for reducer changes

--- src/Async-Load-With-Cancel/Async.hooks.ts
+++ src/Async-Load-With-Cancel/Async.hooks.ts
@@ -1,36 +1,42 @@
-import { useEffect, useReducer, useState } from "react"
+import { useEffect, useReducer } from "react"
 import { asyncReducer } from "./Async.reducer"
-import { asyncEvents, asyncStates, BASE_URL } from "./Async.consts"
+import {
+  ABORT_MESSAGE,
+  asyncEvents,
+  asyncStates,
+  BASE_URL,
+  DEFAULT_ASYNC_STATE,
+} from "./Async.consts"
 
-const { input } = asyncStates
+const { input, error } = asyncStates
 const { paramChanged, errored, acknowledged } = asyncEvents
 
 export const useAsync = () => {
-  const [state, dispatch] = useReducer(asyncReducer, input)
-  const [param, setParam] = useState("")
-  const [result, setResult] = useState()
+  const [state, dispatch] = useReducer(asyncReducer, DEFAULT_ASYNC_STATE)
 
-  const acknowledgeError = () => dispatch(acknowledged)
-  const changeParam = (userId: string) => setParam(userId)
+  const acknowledgeError = () => dispatch({ type: acknowledged })
+
+  const changeParam = (userId: string) => {
+    dispatch({ type: paramChanged, payload: userId })
+  }
+
+  const { mode, param } = state
 
   useEffect(() => {
-    if (state === input) {
-      dispatch(paramChanged)
-
+    if (mode === input) {
       const abortController = new AbortController()
 
       fetch(`${BASE_URL}?=${param}`, { signal: abortController.signal })
         .then((res) => res.json())
-        .then(setResult)
-        .catch(() => dispatch(errored))
+        .then(console.log)
+        .catch(() => dispatch({ type: errored }))
 
-      return () => abortController.abort("aborting previous user request")
+      return () => abortController.abort(ABORT_MESSAGE)
     }
-  }, [state, param])
+  }, [mode, param])
 
   return {
     state: { state },
     action: { acknowledgeError, changeParam },
-    result,
   }
 }
