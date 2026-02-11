38eda71f23310ce7f26b1f8f7ed2aa5be11dcb6b
crotch-watch <naps_08_bay@icloud.com>
2026-02-11 18:27:33 +0530

    refactor(reducer): Swapped if checks for a Lookup Table
    If checks were handling given State A + Event -> State B, this was implemented using lookup table completely separating reducers from transitions

--- src/Multi-Form/MultiForm.constants.ts
+++ src/Multi-Form/MultiForm.constants.ts
@@ -40,3 +40,19 @@ export const FORM_EVENTS = {
 } as const
 
 Object.freeze(Events)
+
+const { dashboard, form } = ENROLLER_STATES
+const { OPEN_FORM } = DASHBOARD_EVENTS
+const { SAVE_AND_EXIT, DETAILS_CHECKED, PAYMENT_INFO_PROVIDED, SUBMIT } =
+  FORM_EVENTS
+const { details, paymentInfo, review, final } = FORM_SUB_STATES
+
+export const ENROLLER_MACHINE = {
+  [dashboard]: { [OPEN_FORM]: { mode: form, substate: details } },
+  [form]: {
+    [SAVE_AND_EXIT]: { mode: dashboard },
+    [DETAILS_CHECKED]: { mode: form, substate: paymentInfo },
+    [PAYMENT_INFO_PROVIDED]: { mode: form, substate: review },
+    [SUBMIT]: { mode: form, substate: final },
+  },
+}
--- src/Multi-Form/MultiForm.reducer.ts
+++ src/Multi-Form/MultiForm.reducer.ts
@@ -1,29 +1,19 @@
-import { Events, ENROLLER_STATES, FORM_SUB_STATES } from "./MultiForm.constants"
+import { ENROLLER_STATES, ENROLLER_MACHINE } from "./MultiForm.constants"
 import type { EnrollerEvents, MachineState } from "./MultiForm.types"
 
-const {
-  OPEN_FORM,
-  DETAILS_CHECKED,
-  PAYMENT_INFO_PROVIDED,
-  SUBMIT,
-  SAVE_AND_EXIT,
-} = Events
-
 const { dashboard, form } = ENROLLER_STATES
-
-const { details, paymentInfo, review, final } = FORM_SUB_STATES
 
 export const multiReducer = (
   state: MachineState,
   actions: EnrollerEvents,
 ): MachineState => {
-  const { event, mode: incomingMode } = actions
+  const { event, mode: requiredMode } = actions
   const { mode } = state
 
-  if (incomingMode !== mode) {
+  if (requiredMode !== mode) {
     console.warn(`
       Illegal Transition Attempted:
-      - Event: ${event} is only valid in Mode: "${incomingMode}"
+      - Event: ${event} is only valid in Mode: "${requiredMode}"
       - Current Internal State mode is: "${mode}"
       Action Ignored.
   `)
@@ -31,24 +21,13 @@ export const multiReducer = (
     return state
   }
 
-  switch (incomingMode) {
+  switch (requiredMode) {
     case dashboard: {
-      if (event === OPEN_FORM) return { mode: form, substate: details }
-      return state
+      return ENROLLER_MACHINE[requiredMode][event]
     }
 
     case form: {
-      if (event === SAVE_AND_EXIT) return { mode: dashboard }
-
-      if (event === DETAILS_CHECKED)
-        return { mode: form, substate: paymentInfo }
-
-      if (event === PAYMENT_INFO_PROVIDED)
-        return { mode: form, substate: review }
-
-      if (event === SUBMIT) return { mode: form, substate: final }
-
-      return state
+      return ENROLLER_MACHINE[requiredMode][event]
     }
   }
 }
