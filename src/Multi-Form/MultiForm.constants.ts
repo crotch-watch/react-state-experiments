5ebbd41f137d98be2ac33848bd4f865cd283db27
crotch-watch <naps_08_bay@icloud.com>
2026-02-24 23:34:35 +0530

    refactor(types, consts): Derived all types from EnrollerMachine
    Since all constants strings were present in EnrollerMachine using them to derive Discriminated Unions than using separate enums

--- src/Multi-Form/MultiForm.constants.ts
+++ src/Multi-Form/MultiForm.constants.ts
@@ -1,76 +1,29 @@
 import type { MachineState } from "./MultiForm.types"
 
-export const FORM_SUB_STATES = {
-  details: "details",
-  paymentInfo: "paymentInfo",
-  review: "review",
-  final: "final",
-} as const
-
-Object.freeze(FORM_SUB_STATES)
-
-export const ENROLLER_STATES = {
-  dashboard: "dashboard",
-  form: "form",
-} as const
-
 export const INITIAL_MACHINE_STATE: MachineState = {
   mode: "dashboard",
 } as const
 
-Object.freeze(INITIAL_MACHINE_STATE)
-
-export const Events = {
-  OPEN_FORM: "OPEN_FORM",
-  SAVE_AND_EXIT: "SAVE_AND_EXIT",
-  DETAILS_CHECKED: "DETAILS_CHECKED",
-  PAYMENT_INFO_PROVIDED: "PAYMENT_INFO_PROVIDED",
-  SUBMIT: "SUBMIT",
-} as const
-
-export const DASHBOARD_EVENTS = {
-  OPEN_FORM: "OPEN_FORM",
-} as const
-
-export const FORM_SUB_STATE_EVENTS = {
-  DETAILS_CHECKED: "DETAILS_CHECKED",
-  PAYMENT_INFO_PROVIDED: "PAYMENT_INFO_PROVIDED",
-  SUBMIT: "SUBMIT",
-} as const
-
-export const FORM_EVENTS = {
-  SAVE_AND_EXIT: "SAVE_AND_EXIT",
-} as const
-
-export const FORM_MACHINE_EVENTS = {
-  SAVE_AND_EXIT: "SAVE_AND_EXIT",
-  ...FORM_SUB_STATE_EVENTS,
-} as const
-
-Object.freeze(Events)
-
-const { dashboard, form } = ENROLLER_STATES
-const { OPEN_FORM } = DASHBOARD_EVENTS
-const { SAVE_AND_EXIT } = FORM_EVENTS
-const { DETAILS_CHECKED, PAYMENT_INFO_PROVIDED, SUBMIT } = FORM_SUB_STATE_EVENTS
-const { details, paymentInfo, review, final } = FORM_SUB_STATES
-
 export const ENROLLER_MACHINE = {
   states: {
-    [dashboard]: { [OPEN_FORM]: { mode: form, substate: details } },
-    [form]: {
-      [SAVE_AND_EXIT]: { mode: dashboard },
+    dashboard: { OPEN_FORM: { mode: "form", substate: "details" } },
+    form: {
+      SAVE_AND_EXIT: { mode: "dashboard" },
       states: {
-        [details]: {
-          [DETAILS_CHECKED]: { mode: form, substate: paymentInfo },
+        details: {
+          event: "DETAILS_CHECKED",
+          target: { mode: "form", substate: "paymentInfo" },
         },
-        [paymentInfo]: {
-          [PAYMENT_INFO_PROVIDED]: { mode: form, substate: review },
+        paymentInfo: {
+          event: "PAYMENT_INFO_PROVIDED",
+          target: { mode: "form", substate: "review" },
         },
-        [review]: {
-          [SUBMIT]: { mode: form, substate: final },
+        review: {
+          event: "SUBMIT",
+          target: { mode: "form", substate: "final" },
         },
+        final: { event: null, target: null },
       },
     },
   },
-}
+} as const
--- src/Multi-Form/MultiForm.types.ts
+++ src/Multi-Form/MultiForm.types.ts
@@ -1,52 +1,26 @@
-import {
-  DASHBOARD_EVENTS,
-  ENROLLER_STATES,
-  FORM_EVENTS,
-  FORM_SUB_STATE_EVENTS,
-  type FORM_SUB_STATES,
-} from "./MultiForm.constants"
+import { ENROLLER_MACHINE } from "./MultiForm.constants"
 
-type FormSubState = keyof typeof FORM_SUB_STATES
-const { dashboard, form } = ENROLLER_STATES
-type Form = typeof form
-type Dashboard = typeof dashboard
+type SubStatesEventMap = typeof ENROLLER_MACHINE.states.form.states
+type SubStates = keyof SubStatesEventMap
+type DashboardEvent = keyof typeof ENROLLER_MACHINE.states.dashboard
 
-export type MachineState =
-  | {
-      mode: Dashboard
-    }
-  | {
-      mode: Form
-      substate: FormSubState
-    }
+type FormStates = {
+  [SubState in SubStates]: { mode: "form"; substate: SubState }
+}[SubStates]
 
-const { OPEN_FORM } = DASHBOARD_EVENTS
-type OPEN_FORM = typeof OPEN_FORM
+export type MachineState = { mode: "dashboard" } | FormStates
 
-type DashBoardTransition = {
-  mode: Dashboard
-  event: OPEN_FORM
+type FormTransitions = {
+  [SubState in SubStates]: {
+    mode: "form"
+    substate: SubState
+    event: SubStatesEventMap[SubState]["event"] | "SAVE_AND_EXIT"
+  }
+}[SubStates]
+
+type DashboardTransition = {
+  mode: "dashboard"
+  event: DashboardEvent
 }
 
-export type EnorollerTable =
-  | DashBoardTransition
-  | {
-      mode: Form
-      substate: FormSubState
-      event: typeof FORM_EVENTS.SAVE_AND_EXIT
-    }
-  | {
-      mode: Form
-      substate: typeof FORM_SUB_STATES.details
-      event: typeof FORM_SUB_STATE_EVENTS.DETAILS_CHECKED
-    }
-  | {
-      mode: Form
-      substate: typeof FORM_SUB_STATES.paymentInfo
-      event: typeof FORM_SUB_STATE_EVENTS.PAYMENT_INFO_PROVIDED
-    }
-  | {
-      mode: Form
-      substate: typeof FORM_SUB_STATES.review
-      event: typeof FORM_SUB_STATE_EVENTS.SUBMIT
-    }
+export type EnrollerTable = DashboardTransition | FormTransitions
